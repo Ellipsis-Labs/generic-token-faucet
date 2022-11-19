a2f278a35f4342a0c5f4f21f87082820d10af57e
throwbackjams <yli17@alumni.nd.edu>
2022-11-19 14:54:00 -0600

    create mint, airdrop token; program and tests

--- programs/devnet-token-faucet/src/lib.rs
+++ programs/devnet-token-faucet/src/lib.rs
@@ -1,23 +1,108 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Mint, Token, TokenAccount};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod devnet_token_faucet {
+    use anchor_spl::token::MintTo;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_mint(ctx: Context<CreateMint>, _ticker: String, _decimals: u8) -> Result<()> {
+        let mint_authority = &mut ctx.accounts.mint_authority;
+
+        // Assert mint_authority is not already initialized
+        assert!(!mint_authority.is_initialized);
+
+        //Store the mint information in the PDA
+        mint_authority.mint = ctx.accounts.mint.key();
+        mint_authority.is_initialized = true;
+        mint_authority.bump = *ctx.bumps.get("mint_authority").unwrap();
+
+        Ok(())
+    }
+
+    pub fn airdrop_spl(ctx: Context<AirdropSpl>, ticker: String, amount: u64) -> Result<()> {
+        let destination_token_account = &ctx.accounts.destination;
+        let mint_account = &ctx.accounts.mint;
+
+        // Assert that the token account matches the mint account
+        assert_eq!(destination_token_account.mint, mint_account.key());
+
+        token::mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo{
+                    authority: ctx.accounts.mint_authority.to_account_info(),
+                    to: ctx.accounts.destination.to_account_info(),
+                    mint: ctx.accounts.mint.to_account_info()                    
+                },
+                &[&[
+                    "mint-authority".as_bytes(),
+                    ticker.to_lowercase().as_ref(),
+                    &[*ctx.bumps.get("mint_authority").unwrap()]
+                ]]),
+            amount
+        )?;
+
+        msg!("Tokens minted!");
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(ticker: String, decimals: u8)]
+pub struct CreateMint<'info>{
+    #[account(
+        init,
+        seeds = ["mint".as_bytes(), ticker.to_lowercase().as_ref()],
+        bump,
+        payer = payer,
+        mint::decimals = decimals,
+        mint::authority = mint_authority,
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(
+        init,
+        payer = payer,
+        seeds = [b"mint-authority".as_ref(), ticker.to_lowercase().as_ref()],
+        bump,
+        space = 8 + 32 + 1 + 1
+    )]
+    pub mint_authority: Account<'info, MintData>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>
+}
+
+#[derive(Accounts)]
+#[instruction(ticker: String)]
+pub struct AirdropSpl<'info>{
+    #[account(
+        mut,
+        seeds = ["mint".as_bytes(), ticker.to_lowercase().as_ref()],
+        bump   
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(
+        seeds = [b"mint-authority".as_ref(), ticker.to_lowercase().as_ref()],
+        bump,
+    )]
+    pub mint_authority: Account<'info, MintData>,
+    #[account(mut)]
+    pub destination: Account<'info, TokenAccount>,
+    pub token_program: Program<'info, Token>,
+}
 
 
 #[account]
 #[derive(Debug)]
 pub struct MintData {
     pub mint: Pubkey,
-    pub bump: u8
+    pub bump: u8,
+    pub is_initialized: bool
 }
--- tests/devnet-token-faucet.ts
+++ tests/devnet-token-faucet.ts
@@ -1,16 +1,87 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { getOrCreateAssociatedTokenAccount, getAccount } from "@solana/spl-token";
+import { expect } from "chai";
+const { SystemProgram, Transaction } = anchor.web3;
 import { DevnetTokenFaucet } from "../target/types/devnet_token_faucet";
+const { TOKEN_PROGRAM_ID, Token, ASSOCIATED_TOKEN_PROGRAM_ID } = require("@solana/spl-token");
 
 describe("devnet-token-faucet", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider);
+
+  const payer = anchor.web3.Keypair.generate();
+  console.log("Payer Pubkey: ", payer.publicKey.toBase58())
 
   const program = anchor.workspace.DevnetTokenFaucet as Program<DevnetTokenFaucet>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  let ticker = "SOL"
+  let ticker_lowercase = ticker.toLowerCase()
+  let decimals = 9
+  let amount_to_mint = 10 * 10^(decimals) 
+
+  it("Create mint", async () => {
+    const signature = await provider.connection.requestAirdrop(payer.publicKey, 1_000_000_000)
+    await provider.connection.confirmTransaction(signature, 'confirmed')
+
+    const [mint] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("mint"),Buffer.from(ticker_lowercase)],
+      program.programId
+    )
+
+    const [mintAuthority] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("mint-authority"), Buffer.from(ticker_lowercase)],
+      program.programId
+    )
+
+    const tx = await program.methods
+      .createMint(ticker,decimals)
+      .accounts({
+        mint,
+        mintAuthority,
+        payer: payer.publicKey,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([payer])
+      .rpc();
     console.log("Your transaction signature", tx);
+
+    const mint_pda = await program.account.mintData.fetch(mintAuthority)
+    expect(mint_pda.isInitialized)
+    expect(mint_pda.mint == mint)
+  });
+
+  it("Mint tokens", async () => {
+    const [mint] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("mint"),Buffer.from(ticker_lowercase)],
+      program.programId
+    )
+
+    const [mintAuthority] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("mint-authority"), Buffer.from(ticker_lowercase)],
+      program.programId
+    )
+
+    const destination = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      payer,
+      mint,
+      TOKEN_PROGRAM_ID,
+    )
+    
+    const ix = await program.methods
+      .airdropSpl(ticker, new anchor.BN(amount_to_mint))
+      .accounts({
+        mint,
+        mintAuthority,
+        destination: destination.address,
+        tokenProgram: TOKEN_PROGRAM_ID
+      })
+      .rpc({skipPreflight: true});
+
+    const destionationTokenAccount = await getAccount(provider.connection, destination.address)
+    expect(Number(destionationTokenAccount.amount)).to.equal(amount_to_mint)
   });
 });
