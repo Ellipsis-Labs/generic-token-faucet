852212537211e89fefaed07d3e5f51014ec5fe34
throwbackjams <yli17@alumni.nd.edu>
2022-11-20 20:21:56 -0600

    add test for deterministic ticker input -> mint creation

--- tests/devnet-token-faucet.ts
+++ tests/devnet-token-faucet.ts
@@ -1,7 +1,7 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { AnchorError, Program } from "@project-serum/anchor";
 import { getOrCreateAssociatedTokenAccount, getAccount } from "@solana/spl-token";
-import { expect } from "chai";
+import { assert, expect } from "chai";
 const { SystemProgram, Transaction } = anchor.web3;
 import { DevnetTokenFaucet } from "../target/types/devnet_token_faucet";
 const { TOKEN_PROGRAM_ID, Token, ASSOCIATED_TOKEN_PROGRAM_ID } = require("@solana/spl-token");
@@ -83,5 +83,56 @@ describe("devnet-token-faucet", () => {
 
     const destionationTokenAccount = await getAccount(provider.connection, destination.address)
     expect(Number(destionationTokenAccount.amount)).to.equal(amount_to_mint)
+  });
+
+  it("Creating mint for an existing ticker should fail", async () => {
+    const signature = await provider.connection.requestAirdrop(payer.publicKey, 1_000_000_000)
+    await provider.connection.confirmTransaction(signature, 'confirmed')
+
+    const [mint] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("mint"),Buffer.from(ticker_lowercase)],
+      program.programId
+    )
+
+    const [mintAuthority] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("mint-authority"), Buffer.from(ticker_lowercase)],
+      program.programId
+    )
+
+    const tx = await program.methods
+      .createMint(ticker,decimals)
+      .accounts({
+        mint,
+        mintAuthority,
+        payer: payer.publicKey,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([payer])
+      .rpc();
+    console.log("Your transaction signature", tx);
+
+    const mint_pda = await program.account.mintData.fetch(mintAuthority)
+    expect(mint_pda.isInitialized)
+    expect(mint_pda.mint == mint)
+
+    try {
+      const secondTx = await program.methods
+        .createMint(ticker,decimals)
+        .accounts({
+          mint,
+          mintAuthority,
+          payer: payer.publicKey,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          systemProgram: SystemProgram.programId,
+        })
+        .signers([payer])
+        .rpc
+      assert.ok(false)
+    } catch (_err) {
+      assert.isTrue(_err instanceof AnchorError);
+          const err: AnchorError = _err;
+          console.log("Error on re-initialization: ", err)
+    }
   });
 });
